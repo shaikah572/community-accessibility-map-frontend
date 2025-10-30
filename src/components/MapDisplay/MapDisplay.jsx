b5175dfeef20d72570a9e1198dd251e0faebbb6f
shaikahalru <sha101kah@gmail.com>
2025-10-30 13:50:54 +0300

    displayed markers on the map

--- src/components/MapDisplay/MapDisplay.jsx
+++ src/components/MapDisplay/MapDisplay.jsx
@@ -11,12 +11,17 @@ const MapDisplay = () => {
 
     const [markers, setMarkers] = useState([])
 
-    const getMarkers = async () =>{
+    const icontest = new Icon({
+        iconUrl: "https://thumbs.dreamstime.com/b/pin-point-location-icon-isolated-white-background-location-icon-141743215.jpg",
+        iconSize: [30, 30]
+    })
+
+    const getMarkers = async () => {
         try {
             const response = await axios.get('http://127.0.0.1:8000/api/markers/')
             console.log(response.data)
             setMarkers(response.data)
-        } catch (error){
+        } catch (error) {
             console.log(error)
         }
     }
@@ -30,11 +35,17 @@ const MapDisplay = () => {
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-                <Marker position={[51.505, -0.09]}>
-                    <Popup>
-                        test
-                    </Popup>
-                </Marker>
+            {
+                markers.map((marker) => {
+                    const customIcon = new Icon({
+                            iconUrl: `http://127.0.0.1:8000/${marker.category.icon}`,
+                            iconSize: [30, 30]
+                        })
+                    return <Marker
+                        position={[marker.lat, marker.lng]} icon={customIcon}
+                    >{marker.name}</Marker>
+                })
+            }
         </MapContainer>
     )
 }
