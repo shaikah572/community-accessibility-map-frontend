3860bbbd85c2270cf4c587a2a301ee22b323c9c4
shaikahalru <sha101kah@gmail.com>
2025-10-30 12:36:53 +0300

    get markers from database to MapDisplay component

--- src/components/MapDisplay/MapDisplay.jsx
+++ src/components/MapDisplay/MapDisplay.jsx
@@ -1,13 +1,29 @@
 // react leaflet > "https://react-leaflet.js.org/"
 // React Leaflet Tutorial for Beginners (2025) > "https://www.youtube.com/watch?v=jD6813wGdBA&t=913s"
 
-import React from 'react'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 import { Icon } from 'leaflet'
 
 const MapDisplay = () => {
 
+    const [markers, setMarkers] = useState([])
+
+    const getMarkers = async () =>{
+        try {
+            const response = await axios.get('http://127.0.0.1:8000/api/markers/')
+            console.log(response.data)
+            setMarkers(response.data)
+        } catch (error){
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        getMarkers()
+    }, [])
 
     return (
         <MapContainer center={[21.506441, 39.196172]} zoom={13}>
