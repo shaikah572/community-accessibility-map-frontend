63613a56e4b05bb623df62215818b4c2233c6926
shaikahalru <sha101kah@gmail.com>
2025-10-31 13:41:49 +0300

    created and added sliding pane when clicking on marker

--- src/components/MapDisplay/MapDisplay.jsx
+++ src/components/MapDisplay/MapDisplay.jsx
@@ -1,20 +1,22 @@
 // react leaflet > "https://react-leaflet.js.org/"
 // React Leaflet Tutorial for Beginners (2025) > "https://www.youtube.com/watch?v=jD6813wGdBA&t=913s"
+// react-sliding-pane > "https://www.npmjs.com/package/react-sliding-pane?activeTab=readme"
 
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 import { Icon } from 'leaflet'
+import SlidingPane from "react-sliding-pane"
+import "react-sliding-pane/dist/react-sliding-pane.css"
+
 
 const MapDisplay = () => {
 
+    const [isPaneOpen, setIsPaneOpen] = useState(false)
     const [markers, setMarkers] = useState([])
+    const [currentMarker, setCurrentMarker] = useState(null)
 
-    const icontest = new Icon({
-        iconUrl: "https://thumbs.dreamstime.com/b/pin-point-location-icon-isolated-white-background-location-icon-141743215.jpg",
-        iconSize: [30, 30]
-    })
 
     const getMarkers = async () => {
         try {
@@ -30,23 +32,45 @@ const MapDisplay = () => {
         getMarkers()
     }, [])
 
+
     return (
-        <MapContainer center={[21.506441, 39.196172]} zoom={13}>
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-            {
-                markers.map((marker) => {
-                    const customIcon = new Icon({
-                            iconUrl: `http://127.0.0.1:8000/${marker.category.icon}`,
-                            iconSize: [30, 30]
+        <>
+                <MapContainer center={[21.506441, 39.196172]} zoom={13} className='map'>
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                    {
+                        markers.map((marker) => {
+                            const customIcon = new Icon({
+                                iconUrl: `http://127.0.0.1:8000/${marker.category.icon}`,
+                                iconSize: [30, 30]
+                            })
+                            return <div>
+                                <Marker
+                                    position={[marker.lat, marker.lng]}
+                                    icon={customIcon}
+                                    eventHandlers={{
+                                        click: () => { setIsPaneOpen(true); setCurrentMarker(marker); },
+                                    }} >
+                                </Marker>
+                            </div>
                         })
-                    return <Marker
-                        position={[marker.lat, marker.lng]} icon={customIcon}
-                    >{marker.name}</Marker>
-                })
-            }
-        </MapContainer>
+                    }
+                </MapContainer>
+            <SlidingPane
+                className='some-custom-class'
+                overlayClassName='some-custom-overlay-class'
+                isOpen={isPaneOpen}
+                title={currentMarker ? currentMarker.name : ''}
+                subtitle={currentMarker ? currentMarker.description : ''}
+                from='left'
+                width='40vw'
+                onRequestClose={() => { setIsPaneOpen(false) }}>
+                <div>test</div>
+                <br />
+                <div>another test</div>
+            </SlidingPane>
+        </>
     )
 }
 
