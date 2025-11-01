0b5fe7dd4c1dc8e25587e8292cfeef22b4799692
shaikahalru <sha101kah@gmail.com>
2025-11-01 13:34:35 +0300

    added logout

---
+++ src/components/Auth/Logout.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { clearTokens } from '../../lib/auth'
+import { useNavigate } from 'react-router'
+
+const Logout = () => {
+    const navigate = useNavigate()
+    function handleLogOut() {
+        clearTokens()
+        setUser(null)
+        navigate('/login')
+    }
+    return (
+        <div>
+            <button onClick={handleLogOut}>Log Out</button>
+        </div>
+    )
+}
+
+export default Logout
